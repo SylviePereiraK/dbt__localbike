ddba89508f1f9cb6f2953fd61e27c7d1a201b36f
Sylvie Pereira <sylvie.pereira01@hotmail.fr>
2025-12-15 17:15:35 +0000

    fix

--- models/intermediate/localbike/int_localbike__products_sold_by_brand_cat.sql
+++ models/intermediate/localbike/int_localbike__products_sold_by_brand_cat.sql
@@ -5,7 +5,14 @@ SELECT
     SUM(quantity_product_sold) OVER (PARTITION BY brand_id) AS quantity_sold_by_brand,
     SUM(price_product_sold) OVER (PARTITION BY brand_id) AS price_sold_by_brand,
     SUM(quantity_product_sold) OVER (PARTITION BY category_id) AS quantity_sold_by_category,
-    SUM(price_product_sold) OVER (PARTITION BY category_id) AS price_sold_by_category
+    SUM(price_product_sold) OVER (PARTITION BY category_id) AS price_sold_by_category,
+    ROW_NUMBER() OVER (ORDER BY SUM(quantity_product_sold) OVER (PARTITION BY brand_id) DESC) AS first_brand_quantity,
+    ROW_NUMBER() OVER (ORDER BY SUM(price_product_sold) OVER (PARTITION BY brand_id) DESC) AS first_brand_price,
+    ROW_NUMBER() OVER (ORDER BY SUM(quantity_product_sold) OVER (PARTITION BY category_id) DESC) AS first_category_quantity,
+    ROW_NUMBER() OVER (ORDER BY SUM(price_product_sold) OVER (PARTITION BY category_id) DESC) AS first_category_price
 FROM {{ref('int_localbike__products_sold')}} ps
 INNER JOIN {{ref('stg_localbike__products')}} p
 ON ps.product_id = p.product_id
+
+
+
---
+++ models/mart/mrt_localbike/mrt_localbike__daily_report.md
@@ -0,0 +1,6 @@
+{% docs mrt_daily_report %}
+
+This model generates a daily summary of orders, including metrics:
+This allows for analysis of performance.
+
+{% enddocs %}
