1ee17660570dc026577a59879de9de53ad2b8443
Sylvie Pereira <sylvie.pereira01@hotmail.fr>
2025-12-16 09:35:59 +0000

    final

--- models/intermediate/localbike/int_localbike__products_sold_by_brand_cat.sql
+++ models/intermediate/localbike/int_localbike__products_sold_by_brand_cat.sql
@@ -1,3 +1,4 @@
+WITH brand_totals AS (
 SELECT 
     p.product_id,
     brand_id,
@@ -5,14 +6,17 @@ SELECT
     SUM(quantity_product_sold) OVER (PARTITION BY brand_id) AS quantity_sold_by_brand,
     SUM(price_product_sold) OVER (PARTITION BY brand_id) AS price_sold_by_brand,
     SUM(quantity_product_sold) OVER (PARTITION BY category_id) AS quantity_sold_by_category,
-    SUM(price_product_sold) OVER (PARTITION BY category_id) AS price_sold_by_category,
-    ROW_NUMBER() OVER (ORDER BY SUM(quantity_product_sold) OVER (PARTITION BY brand_id) DESC) AS first_brand_quantity,
-    ROW_NUMBER() OVER (ORDER BY SUM(price_product_sold) OVER (PARTITION BY brand_id) DESC) AS first_brand_price,
-    ROW_NUMBER() OVER (ORDER BY SUM(quantity_product_sold) OVER (PARTITION BY category_id) DESC) AS first_category_quantity,
-    ROW_NUMBER() OVER (ORDER BY SUM(price_product_sold) OVER (PARTITION BY category_id) DESC) AS first_category_price
+    SUM(price_product_sold) OVER (PARTITION BY category_id) AS price_sold_by_category
 FROM {{ref('int_localbike__products_sold')}} ps
 INNER JOIN {{ref('stg_localbike__products')}} p
 ON ps.product_id = p.product_id
+)
+SELECT *,
+    ROW_NUMBER() OVER (ORDER BY quantity_sold_by_brand DESC) AS first_brand_quantity,
+    ROW_NUMBER() OVER (ORDER BY price_sold_by_brand DESC) AS first_brand_price,
+    ROW_NUMBER() OVER (ORDER BY quantity_sold_by_category DESC) AS first_category_quantity,
+    ROW_NUMBER() OVER (ORDER BY price_sold_by_category DESC) AS first_category_price
+FROM brand_totals
 
 
 
